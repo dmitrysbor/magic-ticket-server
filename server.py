2e0a296d89af7752edd9d487ef1911a573ee79df
dmitrysbor <dmitrysbor@mail.ru>
2021-04-18 11:45:43 +0300

    Update server.py

--- server.py
+++ server.py
@@ -7,24 +7,29 @@ class MyHandler(BaseHTTPRequestHandler):
         result=urlparse(self.requestline)
         num=result[4].split()[0]
         num=num.split('=')
-        MyHandler.lucky(int(num[1]))
+        res=MyHandler.lucky(int(num[1]))
+        with open('lucky.csv', 'a') as f:
+            if f: s=f.write(num[1]+","+res+"\n")
         return
 
     @staticmethod        
     def lucky(num):
         sum_left = 0
         sum_right = 0        
+        result=""
         for index,item in enumerate(str(num)):
             if index<3:
                 sum_left  += int(item)
             else:
                 sum_right += int(item)                
         if sum_left == sum_right:
-            print('lucky')
+            result='lucky'
         else:
-            print('unlucky')          
+            result='unlucky'
 
-# приложение-сервер принимает запросы вида: GET localhost:8081/lucky?num={number}}, 
+        return result            
+
+# приложение-сервер принимает запросы вида: GET localhost:8081/lucky?num={number}, 
 # где number - это число на проверку счастливое ли число: сумма цифр справа равняется сумме чисел слева
 # запрос можно послать например при помощи Postman
 def main(): 
